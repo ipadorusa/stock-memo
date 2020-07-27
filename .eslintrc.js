5990484a260b27feae97bd41c4948a52ee2289c4
rainguy1 <rainguy1@saramin.co.kr>
2020-07-27 12:52:28 +0900

    필요한 파일 업로드

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'prettier/prettier': [
+			'error',
+			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+			// https://prettier.io/docs/en/options.html
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 120,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto',
+			},
+		],
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+	},
 };
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/proj/work_wskim/2020/WMG-26630/wmg-26630/dist'
+		: '/'
+}
